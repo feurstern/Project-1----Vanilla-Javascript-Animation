6f04d2270439e8868e51a87264ed23bff3644cae
Rioo-Feuerstern <rio-kurniawan@sakura-schutzstaffel.com>
2023-03-23 09:46:56 +0700

    Fixing the animation problem and add some button for channging the state.

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const canvas =  document.getElementById("canvas1");
+const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 
 const canvasWidth = canvas.width = 600;
-const canvasHeight =  canvas.height = 600;
+const canvasHeight = canvas.height = 600;
 
 //sprite width is the result from the actual size the pixel of image's width current image width 6867/12(column)= 573
-const spriteWidhth =575;
+const spriteWidhth = 575;
 //sprite height is the actual of the height from the pciture 5230px height / the number of animation 5230/10
-const spirteHeight= 523;
+const spirteHeight = 523;
+
+
+let playerState = "bite";
 
 /*
 blinking problem
@@ -15,8 +18,8 @@ I encounter the problem when I want to animate the sprite it reach to empty spac
 And I have to make it slowely animate by using gameframe and stage frame.
 
 */
-let frameX=0;
-let frameY=0;
+//let frameX=0;
+//let frameY=0;
 
 
 
@@ -32,46 +35,102 @@ const spritesAnimation = [];
 const animationStates = [
     // create the object properties for each states
     {
-        name:'idle',
-        // The value of frameX is equal how many rows in the states
-        frameX :7
-    },
-   
-    {
-        name:'jump',
-        frameX : 7,
-
+        name: 'idle',
+        // The value of frames is equal how many rows in the states. idle has 7 frames.
+        frames: 7
     },
 
     {
-        name :'run',
-        frameX : 8
+        name: 'jump',
+        frames: 7,
+
     },
 
-    
+    {
+        name: 'fall',
+        frames: 7,
+
+    },
+
+    {
+        name: 'run',
+        frames: 9,
+
+    },
+
+    {
+        name: 'dizzy',
+        frames: 11,
+
+    },
+
+    {
+        name: 'sit',
+        frames: 5,
+
+    },
+    {
+        name: 'roll',
+        frames: 7,
+
+    },
+
+    {
+        name: 'bite',
+        frames: 7,
+
+    },
+
+    {
+        name: 'ko',
+        frames: 12,
+
+    },
+
+    {
+        name: 'gethit',
+        frames: 4,
+
+    },
+
+
+
+
+
 
 
 ];
 
-animationStates.forEach((state)=>{
-    let frames ={
+animationStates.forEach((state, index) => {
+    let frames = {
         loc: [],
     }
-})
+    for (let i = 0; i < state.frames; i++) {
+        let positionX = i * spriteWidhth;
+        let positionY = index * spirteHeight;
+        frames.loc.push({ x: positionX, y: positionY });
 
+
+    }
+    spritesAnimation[state.name] = frames;
+});
+
+console.log(animationStates);
 
 const playerImage = new Image();
-playerImage.src ="./src/pic/shadow_dog.png";
-let x =1;
+playerImage.src = "./src/pic/shadow_dog.png";
+let x = 1;
 
-const animate=()=>{
+const animate = () => {
     //declare position to cycle horizontal sprite and modulus by 6, because it's idle animation contain six animation
-    let position =Math.floor(gameFrame/staggerFrame) %6;
-    frameX = position * spriteWidhth;
+    let position = Math.floor(gameFrame / staggerFrame) % spritesAnimation[playerState].loc.length;
 
-    ctx.clearRect(0,0, canvasWidth, canvasHeight);
+    let frameX = spriteWidhth * position;
+    let frameY = spritesAnimation[playerState].loc[position].y;
+
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
     //ctxdrawimage(image, source x, source y, source height, source width, destination height, destination width)
-    ctx.drawImage(playerImage, frameX, frameY*spirteHeight, spriteWidhth, spirteHeight,0,0, spriteWidhth, spirteHeight);
+    ctx.drawImage(playerImage, frameX, frameY, spriteWidhth, spirteHeight, 0, 0, spriteWidhth, spirteHeight);
 
     /*
     if(gameFrame % staggerFrame ==0){
@@ -82,7 +141,7 @@ const animate=()=>{
     }
     */
     gameFrame++;
-   
+
     requestAnimationFrame(animate);
 
 
